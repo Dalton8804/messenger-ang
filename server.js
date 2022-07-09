d56fee501956a0a971eb17cf2db83f0f34a9593a
Dalton <Daltonjavery@gmail.com>
2022-07-09 15:54:02 -0500

    updated deployment for heroku

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/messenger-ang'));
+app.use(express.static(__dirname + '/dist/messenger-ang/src'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/messenger-ang/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/messenger-ang/src/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
